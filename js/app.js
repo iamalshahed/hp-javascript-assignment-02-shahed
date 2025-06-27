c0fd321ca176cc207dff4ebe26fe9fba61de47a6
iamalshahed <shahedevise@gmail.com>
2025-06-27 22:53:43 +0600

    select DOM elements

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// DOM Elements
+const balanceDisplay = document.getElementById("balance");
+const addBtn = document.getElementById("addBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
+const historyBtn = document.getElementById("historyBtn");
+
+const addForm = document.getElementById("addForm");
+const withdrawForm = document.getElementById("withdrawForm");
+const historySection = document.getElementById("historySection");
+
+const confirmAdd = document.getElementById("confirmAdd");
+const confirmWithdraw = document.getElementById("confirmWithdraw");
+const addAmountInput = document.getElementById("addAmount");
+const withdrawAmountInput = document.getElementById("withdrawAmount");
+const transactionTable = document.getElementById("transactionTable");
